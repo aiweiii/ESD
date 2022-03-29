7d3a81b5988053db30e3975ee2d4afa3067e8da2
asherqjl <quekjiale@gmail.com>
2022-03-29 18:57:34 +0800

    Maars Dont like to pull

--- Compiled/project/init/combinedSql.sql
+++ Compiled/project/init/combinedSql.sql
@@ -16,7 +16,7 @@ GRANT ALL ON `customers`.* TO 'user'@'%';
 USE `customers`;
 -- --------------------------------------------------------
 --
--- Table structure for table `books`
+-- Table structure for table `customers`
 --
 DROP TABLE IF EXISTS `customers`;
 CREATE TABLE IF NOT EXISTS `customers` (
@@ -30,9 +30,9 @@ CREATE TABLE IF NOT EXISTS `customers` (
 -- Dumping data for table `customers`
 --
 INSERT INTO `customers` (`custID`, `custName`, `custAddress`, `custCCNo`) VALUES
-('A01', 'Emma Tan','53 Ang Mo Kio Avenue 3 Singapore 569933', '978112947'),
-('A02', 'Yvonne Kim', 'Blk 145 Lorong 2 Toa Payoh Singapore 310145', '978134947'),
-('A03', 'Michael Ang', 'Blk 150A Bishan Street 11 Singapore 571150', '978143447');
+('1111', 'Emma Tan','53 Ang Mo Kio Avenue 3 Singapore 569933', '978112947'),
+('2222', 'Yvonne Kim', 'Blk 145 Lorong 2 Toa Payoh Singapore 310145', '978134947'),
+('3333', 'Michael Ang', 'Blk 150A Bishan Street 11 Singapore 571150', '978143447');
 COMMIT;
 --
 -- ======================= CUSTOMERS END ========================== --
@@ -51,68 +51,72 @@ CREATE TABLE IF NOT EXISTS `seller` (
   `sellerName` varchar(32) NOT NULL,
   `sellerCtcNo` int(11) NOT NULL,
   `sellerBankAccNo` int(24) NOT NULL,
+  `telegramId` VARCHAR(24) NOT NULL, 
+  `chatId` int(24) NOT NULL,
   PRIMARY KEY (`sellerID`)
 ); 
 --
 -- Dumping data for table `seller`
 --
-INSERT INTO `seller` (`sellerID`, `sellerName`, `sellerCtcNo`, `sellerBankAccNo`) VALUES
-('1', 'Cindy', '98989898', '774777773'),
-('1', 'Cindy', '98989898', '774777773');
+INSERT INTO `seller` (`sellerID`, `sellerName`, `sellerCtcNo`, `sellerBankAccNo`,`telegramId`,`chatId`) VALUES
+('1', 'Cindy', '98989898', '7747332773','maars505',93366384),
+('2', 'Candy', '91919191', '7747733573','maars505',93366384);
 COMMIT;
 --
 -- ======================= SELLER END ========================== --
 
 -- -- ======================= ORDER ========================== --
-CREATE DATABASE IF NOT EXISTS `ORDER`;
-GRANT ALL ON `ORDER`.* TO 'USER'@'%';
+CREATE DATABASE IF NOT EXISTS `orders`;
 -- ADDED THIS PART TO CREATE MULTIPLE DATABASES:
-USE `ORDER`;
+USE `orders`;
 -- --------------------------------------------------------
 --
 -- TABLE STRUCTURE FOR TABLE `ORDER`
 --
-DROP TABLE IF EXISTS `ORDER`;
-CREATE TABLE IF NOT EXISTS `ORDER` (
-  `ORDER_ID` INT(11) NOT NULL AUTO_INCREMENT,
-  `CUSTOMER_ID` VARCHAR(32) NOT NULL,
-  `STATUS` VARCHAR(10) NOT NULL DEFAULT 'NEW',
-  `CREATED` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `MODIFIED` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`ORDER_ID`)
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE IF NOT EXISTS `order` (
+  `order_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `customerID` VARCHAR(11) NOT NULL,
+  `status` VARCHAR(34) NOT NULL DEFAULT "Payment Completed",
+  `dateOfPurchase` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `dateOfModification` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`order_id`)
 );
 --
 -- DUMPING DATA FOR TABLE `ORDER`
 --
-INSERT INTO `ORDER` (`ORDER_ID`, `CUSTOMER_ID`, `STATUS`, `CREATED`, `MODIFIED`) VALUES
-(1, 'APPLE TAN', 'NEW', '2020-06-12 02:14:55', '2020-06-12 02:14:55');
+INSERT INTO `order` (`order_id`, `customerID`, `status`, `dateOfPurchase`, `dateOfModification`) VALUES
+(1, '1111', 'NEW', '2020-06-12 02:14:55', '2020-06-12 02:14:55'),
+(2, '2222', 'NEW', '2020-06-12 02:14:55', '2020-06-12 02:14:55');
 
 -- --------------------------------------------------------
 --
 -- TABLE STRUCTURE FOR TABLE `ORDER_ITEM`
 --
-DROP TABLE IF EXISTS `ORDER_ITEM`;
-CREATE TABLE IF NOT EXISTS `ORDER_ITEM` (
-  `ITEM_ID` INT(11) NOT NULL AUTO_INCREMENT,
-  `ORDER_ID` INT(11) NOT NULL,
-  `BOOK_ID` CHAR(13) NOT NULL,
-  `QUANTITY` INT(11) NOT NULL,
-  PRIMARY KEY (`ITEM_ID`),
-  KEY `FK_ORDER_ID` (`ORDER_ID`)
-) ENGINE=INNODB AUTO_INCREMENT=3 DEFAULT CHARSET=UTF8;
+DROP TABLE IF EXISTS `order_item`;
+CREATE TABLE IF NOT EXISTS `order_item` (
+  `order_item_id` INT(11) NOT NULL  AUTO_INCREMENT,
+  `order_id` INT(11) NOT NULL,
+  `sellerID` INT(11) NOT NULL,
+  `id` INT(11) NOT NULL, 
+  `productName` VARCHAR(32) NOT NULL,
+  `itemPrice` DECIMAL(5,2) NOT NULL,
+  `quantity` INT(11) NOT NULL,
+  PRIMARY KEY (`order_item_id`),
+  KEY `FK_order_item_id` (`order_id`)
+);
 --
 -- DUMPING DATA FOR TABLE `ORDER_ITEM`
 --
-INSERT INTO `ORDER_ITEM` (`ITEM_ID`, `ORDER_ID`, `BOOK_ID`, `QUANTITY`) VALUES
-(1, 1, '9781434474234', 1),
-(2, 1, '9781449474212', 1);
+INSERT INTO `order_item` (`order_item_id`, `order_id`, `sellerID`, `productName`, `itemPrice`,`quantity`) VALUES
+(1, 1, 1, 'MaarsBar', 100.29, 10),
+(2, 1, 1, 'AiWeiJack', 20.29, 10);
 --
 -- CONSTRAINTS FOR DUMPED TABLES
 --
 --
 -- CONSTRAINTS FOR TABLE `ORDER_ITEM`
 --
-ALTER TABLE `ORDER_ITEM`
-  ADD CONSTRAINT `FK_ORDER_ID` FOREIGN KEY (`ORDER_ID`) REFERENCES `ORDER` (`ORDER_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
--- ======================= ORDER END ========================== --
+ALTER TABLE `order_item`
+  ADD CONSTRAINT `order_id` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- -- ======================= ORDER END ========================== --
---
+++ Compiled/project/order.Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3-slim
+WORKDIR /usr/src/app
+COPY requirements.txt ./
+RUN python -m pip install --upgrade pip
+RUN python -m pip install --no-cache-dir -r requirements.txt
+COPY ./seller.py ./
+CMD [ "python", "./order.py" ]
